f91db09ae6cd4a21202c15d6f5d9eda592c869c4
Hano <hinsul3113@gmail.com>
2019-03-01 10:50:50 -0800

    Created a simple context used default value

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  // Bellow we are adding contextType property to Button class
+  static contextType = LanguageContext; // 👈 this is equal to: Button.contextType = LanguageContext (but, Button.property should be called outside the Button class brackets)
   render() {
+    console.log(this.context);
     return <button className="ui button primary">Submit</button>
   }
 }
