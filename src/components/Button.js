286b77f3be9d3362e5eb265b6ec7f019be057b0c
Hano <hinsul3113@gmail.com>
2019-03-01 11:04:39 -0800

    create a provider to change the context default value

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,8 +5,8 @@ class Button extends React.Component {
   // Bellow we are adding contextType property to Button class
   static contextType = LanguageContext; // 👈 this is equal to: Button.contextType = LanguageContext (but, Button.property should be called outside the Button class brackets)
   render() {
-    console.log(this.context);
-    return <button className="ui button primary">Submit</button>
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return <button className="ui button primary">{text}</button>
   }
 }
 
