5ca101e95599f204d623134c24f6e1b77328c778
Hano <hinsul3113@gmail.com>
2019-03-07 06:20:49 -0800

    used Context.Consumer to get information out of the Context system pipe

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,28 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  // using Provider
   // Bellow we are adding contextType property to Button class
-  static contextType = LanguageContext; // 👈 this is equal to: Button.contextType = LanguageContext (but, Button.property should be called outside the Button class brackets)
+  // static contextType = LanguageContext; // 👈 this is equal to: Button.contextType = LanguageContext (but, Button.property should be called outside the Button class brackets)
+
+  // Using Consumer
+  renderSubmit(value) {
+    return value === 'english'? 'Submit' : 'Voorleggen';
+  }
+
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-    return <button className="ui button primary">{text}</button>
+    // Using Provider
+    // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    // return <button className="ui button primary">{text}</button>
+
+    // Using Consumer
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value) }
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
