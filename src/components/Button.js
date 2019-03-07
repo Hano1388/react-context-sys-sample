ed68cfba23fd5470e1227fb7463b1d6447dea138
Hano <hinsul3113@gmail.com>
2019-03-07 06:33:36 -0800

    added a new context to render button color

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
   // using Provider
@@ -11,6 +12,16 @@ class Button extends React.Component {
     return value === 'english'? 'Submit' : 'Voorleggen';
   }
 
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+        {(value) => this.renderSubmit(value) }
+        </LanguageContext.Consumer>
+      </button>
+    );
+  };
+
   render() {
     // Using Provider
     // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
@@ -18,11 +29,9 @@ class Button extends React.Component {
 
     // Using Consumer
     return (
-      <button className="ui button primary">
-        <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value) }
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+      { (color) => this.renderButton(color) }
+      </ColorContext.Consumer>
     );
   }
 }
